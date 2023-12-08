e349a3cf442d87a7e6a7b5ff5a8579bc15f1be57
Adityatech16 <gajanangaikawad15@gmail.com>
2023-12-08 19:34:49 +0530

    Your commit message here

---
+++ EmlopyeeMaanagementWithJwt/src/main/java/com/app/demo/EmlopyeeMaanagementWithJwtApplication.java
@@ -0,0 +1,13 @@
+package com.app.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmlopyeeMaanagementWithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmlopyeeMaanagementWithJwtApplication.class, args);
+	}
+
+}
